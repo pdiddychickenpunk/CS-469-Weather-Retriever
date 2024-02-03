f0e8454f8b86637c3c7f5d71c081a78e598d926f
pdiddychickenpunk <pdiddychickenpunk@gmail.com>
2024-02-03 16:14:28 -0700

    Add API attribution and begin retrieval function

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@
     Attributions:
 
     “Your Career in Web Development Starts Here.” Your Career in Web Development Starts Here | The Odin Project, The Odin Project, www.theodinproject.com/. Accessed 24 Jan. 2024.
+    “1.4: JSON - Working with Data and Apis in JavaScript.” YouTube, The Coding Train, 25 May 2019, www.youtube.com/watch?v=uxf0--uiX0I. 
 
 */
 
+async function getWeatherData() {
 
-let greeting = 'Hello CS 469!';
-console.log(greeting);
+    const response = await fetch("https://api.weatherapi.com/v1/current.json?key=38c484859aa14682b96214935242001&q=85205&aqi=no");
+
+
+}
