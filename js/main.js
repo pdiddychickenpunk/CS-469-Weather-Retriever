a8cb93950931575e479305bd453e78387d2bc4d5
pdiddychickenpunk <pdiddychickenpunk@gmail.com>
2024-02-03 15:50:42 -0700

    Update attributions

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+/*
+    Attributions:
+
+    “Your Career in Web Development Starts Here.” Your Career in Web Development Starts Here | The Odin Project, The Odin Project, www.theodinproject.com/. Accessed 24 Jan. 2024.
+
+*/
+
+
 let greeting = 'Hello CS 469!';
 console.log(greeting);
